ea0dacf7a5912995c7b570043c904fe2b9c49d57
Alchemist-X <55140230+Alchemist-X@users.noreply.github.com>
2023-04-23 16:59:31 +0800

    Update 88.py

--- 88.py
+++ 88.py
@@ -7,3 +7,6 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        for j in range(n):
+            nums1[m+j] = nums2[j]
+        nums1.sort()
